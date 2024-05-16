4d7d94e7dcb9a52c7add78b69b51856c63b8210c
Rizky <rizkyterm@gmail.com>
2024-05-16 21:01:07 +0700

    add isian

---
+++ isian_question.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        question: "Apa ibu kota Indonesia?",
+        correctAnswer: "jakarta"
+    },
+    {
+        question: "Berapa hasil dari 7 + 5?ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+        correctAnswer: "12"
+    },
+    {
+        question: "siapa nama saya?",
+        correctAnswer: "Fikri"
+    },
+];
